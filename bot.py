17895073e2a211c800565b3f3ba43bbd9c0a41b1
Rebecal Albuquerque <rebeca.albuquerque96@gmail.com>
2022-02-20 22:14:33 -0300

    changing regex

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 import re
 
 PORT = int(os.environ.get('PORT', 8443))
-ADD_REGEX = "(([01]?[0-9]|2[0-3]):[0-5][0-9]);([0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2});([0-9]{1,2});(r|g);(.+?);(\\b[^\\d\\W]+\\b$)"
+ADD_REGEX = "(([01]?[0-9]|2[0-3]):[0-5][0-9]);([0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2});(-?[0-9]{1,2});(r|g);(.+?);(\\b[^\\d\\W]+\\b$)"
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
